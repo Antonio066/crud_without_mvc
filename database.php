ef182e132983eeb90838ecc8967fe0631131f46c
Antonio066 <deeveloper00.1@gmail.com>
2024-05-28 19:35:51 -0400

    Update database.php

--- database.php
+++ database.php
@@ -1,5 +1,8 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "crud_test");
+$u = "INGRESE NOMBRE DE SU USUARIO ENTRE COMILLAS";
+$p = "NOMBRE DE SU CONTRASEÑA EN MAYUSCULAS O MINUSCULAS COMO LO HAY DEFEINIDO, ENTRE COMILLAS";
+$base = "NOMBRE DE LA BASE DE DATOS, IGUAL EN COMILLAS";
+$con = mysqli_connect("localhost", $u, $p, $base);
 // if (isset($con)) {
 //    echo "conexión exitosa";
 // }
